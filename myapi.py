6fdbcaee0326ee8589991b7f0c5758aa75d8a272
Safwat Bushra Afreen <afreenbushra82@gmail.com>
2025-04-25 02:26:35 +0600

    path parameters

--- myapi.py
+++ myapi.py
@@ -1,6 +1,26 @@
-from FastAPI import FastAPI
+from fastapi import FastAPI
 app = FastAPI()
+student = {
+    1: {
+        "name": "hello",
+        "age": 30,
+        "class": 12
+    },
+    2: {
+        "name": "john",
+        "age": 20,
+        "class": 11
+    }
+}
+
 @app.get("/")
-def index()
+def index():
     return{ "name" : "first data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int):
+    if student_id not in student:
+        return {"error": "Student not found"}
+    return student[student_id]
+
 
