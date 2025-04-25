72c1ceb14ec0f597792adcc2d86ab024cb5296cb
Safwat Bushra Afreen <afreenbushra82@gmail.com>
2025-04-25 13:13:36 +0600

    path parameter and query parameter together

--- myapi.py
+++ myapi.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Optional
 app = FastAPI()
 student = {
     1: {
@@ -18,9 +19,17 @@ def index():
     return{ "name" : "first data"}
 
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int):
+def get_student(student_id: int = Path(..., title="Student ID", description="The ID of the student to retrieve")):
+    
     if student_id not in student:
         return {"error": "Student not found"}
     return student[student_id]
+
+@app.get("/get-by-name/{student_id})")
+def get_by_name(*, student_id : int ,name: Optional[str] = None, test :int):
+    for student_id in students:
+        return student[student_id]["name"]== name
+    return {"data": "not found"}
+
 
 
